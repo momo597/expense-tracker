a8f5328de554592e5b50288700ecaeec343497b8
Mohamed Ayouch <ayouchmohamed1999@gmail.com>
2026-05-25 17:17:04 -0400

    Fix Integration test file name

--- src/test/java/com/momo/expense_tracker/ExpenseTrackerControllerIT.java
+++ src/test/java/com/momo/expense_tracker/ExpenseTrackerControllerIT.java
@@ -17,7 +17,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-public class ExpenseTrackerControllerIntegrationTests extends BasePostgresIntegrationTest {
+public class ExpenseTrackerControllerIT extends BasePostgresIntegrationTest {
 
   @Autowired private MockMvc mockMvc;
 
